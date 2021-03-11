a3862812383c3b95df34765cea940e5a4a3c927d
haixiangyan <haixiang6123@gmail.com>
2021-03-11 20:30:50 +0800

    feat: 使用内存缓存

--- middlewares/accessToken.js
+++ middlewares/accessToken.js
@@ -1,23 +1,21 @@
-const fs = require('fs')
-const path = require('path')
 const proxy = require('../proxy')
 
 let accessToken = null
-
-const cachePath = path.join(__dirname, '../cache')
-const accessTokenFilePath = path.join(cachePath, 'access_token.txt')
-const expirationFilePath = path.join(cachePath, 'expiration.txt')
+let expiration = null
 
 const OFFSET = 100;
 
 const getAccessTokenFromCache = () => {
   try {
-    const cacheAccessToken = fs.readFileSync(accessTokenFilePath, 'utf8')
-    const expirationText = fs.readFileSync(expirationFilePath, 'utf8')
+    // 是否已经存在
+    if (!accessToken || !expiration) {
+      return null
+    }
 
-    const expiration = new Date(expirationText)
+    const expireDate = new Date(expiration)
 
-    return Date.now() - expiration.getTime() <= 0 ? null : cacheAccessToken
+    // 判读是否过期
+    return Date.now() - expireDate.getTime() <= 0 ? null : accessToken
   } catch (e) {
     console.error(e)
     return null
@@ -33,16 +31,10 @@ const fetchAccessToken = async () => {
   })
 
   accessToken = response.data.access_token
-  const expireTime = Date.now() + response.data.expires_in - OFFSET
 
-  // TODO: 可以使用 Redis 做缓存
-  // 更新缓存
-  if (!fs.existsSync(cachePath)) fs.mkdirSync(cachePath)
+  expiration = Date.now() + response.data.expires_in - OFFSET
 
-  fs.writeFileSync(accessTokenFilePath, accessToken)
-  fs.writeFileSync(expirationFilePath, expireTime)
-
-  console.log('当前 access_token 为' + accessToken)
+  console.log('获取 access_token: ' + accessToken)
 
   return accessToken
 }
