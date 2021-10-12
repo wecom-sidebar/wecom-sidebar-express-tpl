17c02b9278c3060cfdc51a81fdc6c9d5da4de4f4
haixiangyan <haixiang6123@gmail.com>
2021-10-12 09:37:50 +0800

    feat: 服务端缓存 ticket

--- routes/qywx-utils.js
+++ routes/qywx-utils.js
@@ -5,6 +5,10 @@ const QywxBaseController = require('../controllers/QywxProxyController');
 
 const prefix = '/api/qywx-utils/';
 
+// 缓存的 jsapi ticket
+let tickets = {}
+const OFFSET = 100;
+
 router.prefix(prefix)
 
 const nonceStr = Buffer.from(new Date().toISOString()).toString('base64')
@@ -15,16 +19,33 @@ router.get('/signatures', async (ctx) => {
   const {url} = ctx.request.query;
 
   const parsedUrl = decodeURIComponent(url);
+  const cache = tickets[parsedUrl];
+  let corpTicket, appTicket;
 
-  // 获取企业 jsapi_ticket 和应用 jsapi_ticket
-  const [corpTicketRes, appTicketRes] = await Promise.all([
-    QywxBaseController.getRequest('/get_jsapi_ticket', {}, ctx.accessToken),
-    QywxBaseController.getRequest('/ticket/get', { type: 'agent_config'}, ctx.accessToken)
-  ])
+  // 是否有缓存且是否有过期
+  if (cache && Date.now() - cache.appExpiration < 0) {
+    [corpTicket, appTicket] = [cache.corpTicket, cache.appTicket];
+  } else {
+    // 获取企业 jsapi_ticket 和应用 jsapi_ticket
+    const [corpTicketRes, appTicketRes] = await Promise.all([
+      QywxBaseController.getRequest('/get_jsapi_ticket', {}, ctx.accessToken),
+      QywxBaseController.getRequest('/ticket/get', { type: 'agent_config'}, ctx.accessToken)
+    ]);
+
+    [corpTicket, appTicket] = [corpTicketRes.ticket, appTicketRes.ticket];
+
+    // 写入缓存
+    tickets[parsedUrl] = {
+      appTicket: appTicketRes.ticket,
+      appExpiration: Date.now() + appTicketRes.expires_in - OFFSET,
+      corpTicket: corpTicketRes.ticket,
+      corpExpiration: Date.now() + corpTicketRes.expires_in - OFFSET,
+    }
+  }
 
   // 生成签名
-  const corpSignature = sign(corpTicketRes.ticket, nonceStr, timestamp, parsedUrl)
-  const appSignature = sign(appTicketRes.ticket, nonceStr, timestamp, parsedUrl)
+  const corpSignature = sign(corpTicket, nonceStr, timestamp, parsedUrl)
+  const appSignature = sign(appTicket, nonceStr, timestamp, parsedUrl)
 
   ctx.body = {
     meta: {
@@ -33,13 +54,13 @@ router.get('/signatures', async (ctx) => {
       url: parsedUrl,
     },
     app: {
-      ticket: appTicketRes.ticket,
-      expires: appTicketRes.expires,
+      ticket: appTicket,
+      expires: tickets[parsedUrl].appExpiration,
       signature: appSignature,
     },
     corp: {
-      ticket: corpTicketRes.ticket,
-      expires: corpTicketRes.expires,
+      ticket: corpTicket,
+      expires: tickets[parsedUrl].corpExpiration,
       signature: corpSignature,
     },
   }
